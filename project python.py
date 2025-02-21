f39e596fc8ec03d929e7d0fc698932f36b09fba8
aufaqonita <aufa.qonita@student.alhazen.academy>
2025-02-21 11:01:00 +0700

    project minggu lalu

---
+++ project python.py
@@ -0,0 +1,15 @@
+nama_lengkap = input("masukan nama lengkap")
+email = input("masukan email")
+nomor_hp = input("masukan nomor handphone")
+
+print("=" * 40)
+print("formulir pendaftaran akun siswa")
+print("=" * 40)
+print("\nNama lengkap :", nama_lengkap )
+print("Email :", email)
+print("Nomor handphone :", nomor_hp)
+
+print("\nHi!", nama_lengkap)
+print("Selamat! Email", email, "berhasil terdaftar di siswa kami")
+print("\nlink untuk login ke aplikasi dikirim ke nomor", nomor_hp)
+
