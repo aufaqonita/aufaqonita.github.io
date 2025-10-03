9807bdfde6ed54a7125771ed46a2b1eb137d3b71
aufaqonita <aufa.qonita@student.alhazen.academy>
2025-10-03 10:33:58 +0700

    Add files via upload

--- OOP.py
+++ OOP.py
@@ -1,22 +1,77 @@
-class Animal:
-    name = color = ""
 
-    def __init__(self, animal_name, animal_color):
-        self.name = animal_name
-        self.color = animal_color
-    def makeSound(self, sound):
-        print(self.name, "is", sound)
-    def eat(self, food):
-         print(self.name, "is eating", food)   
+class Person:
+    name = ""
 
-cat = Animal("Max", "yellow")
-cat.makeSound("meowing")
-cat.eat("fish")
+    def__init__(self, name);
+    self.name = name
+    
+    def getInfo(self):
+        print("this person's name", self.name)
 
-cat = Animal("Miu", "grey")
-cat.makeSound("barking")
-cat.eat("snacks")  
 
-bird = Animal("Tweety", "blue")
-cat.makeSound("chirping")
-cat.eat("corn")         
+class Person:
+    name = ""
+
+    def__init__(self, name);
+    self.name = name
+    
+    def getInfo(self):
+        print("this person's name", self.name)
+
+
+
+class Student(Person):
+   pass
+student1 = Student("Ahmad")
+student.getInfo      
+
+class Person:
+    name = ""
+
+    def__init__(self, name);
+    self.name = name
+    
+    def getInfo(self):
+        print("this person's name", self.name)
+
+
+
+class Student(Person):
+    def__init__(self, name, nis)
+    self.name = name
+    self.nis = nis
+Person1 = Person("Ahmad")
+Student1= Student("Beni","21211243")
+
+class Student(Person):
+    def__init__(self, name, nis)
+    super().__init__(name)
+    self.nis = nis
+Person1 = Person("Ahmad")
+Student1= Student("Beni","21211243")
+
+class Student(Person):
+    nis = ""
+
+    def__init__(self, name, nis)
+    super( ).__init__(name)
+    self.nis = nis
+    def study(subject):
+       print(self.name, "is studying", subject)
+
+Student1= Student("Beni", "20211243") 
+Student1.study("Math")
+
+class Student(Person):
+
+    def__init__(self, name, nis)
+    super( ).__init__(name)
+    self.nis = nis
+
+    def study(subject):
+       print(self.name, "is studying", subject)
+    def getInfo():
+        print("This student's name is", self.name)  
+
+Student1= Student("Beni", "20211243") 
+Student1.getInfo()        
