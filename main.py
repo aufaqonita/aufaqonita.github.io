8f9fd4492ec127e274cbd85aad3a9cabcc78ee61
aufaqonita <aufa.qonita@student.alhazen.academy>
2025-10-10 14:23:37 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import mathemathics
+
+mathemathics.area_of_square(50)
+mathemathics.area_of_triangle(8, 12)
+
+
+import mathemathics as mm
+
+mm.area_of_square(50)
+mm.area_of_triangle(8, 12)
+
+from mathemathics import area_of_triangle
+area_of_triangle(8, 12)
+
+from mathematics.square.area import square_area
+from mathematics.square.area import triangle_area
+from mathematics.square.area import circle_area
+
+square_area(15)
+triangle_area(10, 15)
+circle_area(12)
