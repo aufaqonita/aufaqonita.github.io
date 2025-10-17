09cc700f3d52757fadc2b088f3574ce8dd2107ac
aufaqonita <aufa.qonita@student.alhazen.academy>
2025-10-17 13:18:41 +0700

    Add files via upload

---
+++ pip.py
@@ -0,0 +1,7 @@
+from stringcolor import *
+
+print(cs("Selamat datang", "blue", "white"))
+print(bold("Selamat datang"))
+print(underline("Selamat datang"))
+print(underline("Selamat datang").bold())
+print(cs("Selamat datang", "blue", "yellow").underline())
